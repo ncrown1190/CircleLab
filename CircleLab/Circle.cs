a3d44cf1754a97d1cbfe831858768aeda18abff3
ncrown1190 <ncrown88@outlook.com>
2024-11-28 15:33:50 -0500

    initial commit

---
+++ CircleLab/Circle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleLab
+{
+    internal class Circle
+    {
+        // Properties
+        private double Radius { get; set; }
+
+        //  Constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public void GetPositiveNumber(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Invalid data. Please try again");
+            }
+            Radius = radius;
+        }
+
+        //  Methods
+        // 01 to calculate Diameter
+        public double CalculateDiameter()
+        {
+            return Radius * 2;
+        }
+        // 02 to calculate circumference
+        public double CalculateCircumference()
+        {
+            double result = Math.PI * Radius * 2;
+            result = Math.Round(result, 2);
+            return result;
+        }
+
+        // 03 to calculate Area
+        public double CalculateArea()
+        {
+            double result = Math.Round(Math.PI * (Radius * Radius), 2);
+            return result;
+            //return Math.PI * Math.Pow(Radius, 2); 
+        }
+
+        //  The grow method will adjust radius to double its value.
+        public void Grow()
+        {
+            Radius *= 2;
+        }
+
+        public double GetRadius()
+        {
+            return Radius;
+        }
+    }
+}
---
+++ CircleLab/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CircleLab;
+
+Console.WriteLine("Hello, World!");
+
+/*  The application prompts the user to enter a radius; the user may enter a number with decimals (double) */
+Console.WriteLine("Welcome to the Circle Tester");
+Circle circleObject = new Circle(0);
+Console.Write("Enter radius: ");
+while (true)
+{
+    double radius = double.Parse(Console.ReadLine());
+    circleObject = new Circle(radius);
+    try
+    {
+        circleObject.GetPositiveNumber(radius);
+        break;
+    }
+    catch (ArgumentException)
+    {
+        //circleObject.GetPositiveNumber(6.0);
+        Console.Write("Invalid number. Try again: ");
+    }
+}
+CircleCalculations();
+
+while (true)
+{
+    Console.Write("Should the circle grow? Y/N: ");
+    string userAnswer = Console.ReadLine();
+
+    if (userAnswer.ToLower() == "y")
+    {
+        Console.WriteLine("The circle is magically growing.");
+        circleObject.Grow();
+        //Console.WriteLine($"Diameter: {circleObject.CalculateDiameter()}");
+        CircleCalculations();
+        //Console.WriteLine(radius);
+    }
+    else
+    {
+        Console.WriteLine($"Goodbye! The circle's final radius is {circleObject.GetRadius()} ");
+        break;
+    }
+}
+void CircleCalculations()
+{
+    Console.WriteLine($"Diameter: {circleObject.CalculateDiameter()}");
+    Console.WriteLine($"Circumference: {circleObject.CalculateCircumference()}");
+    Console.WriteLine($"Area: {circleObject.CalculateArea()}");
+}
+
+Console.WriteLine("Press any key to quit.");
+Console.ReadKey();
